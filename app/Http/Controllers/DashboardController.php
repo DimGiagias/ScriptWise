6a6093420d431d35c106c5b944c6ddd488cf37f0
DimGiagias <dim71gia@gmail.com>
2025-05-21 21:48:40 +0300

    feat: display quiz attempts in dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class DashboardController extends Controller
+{
+    public function index(): Response
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return Inertia::render('Dashboard', ['quizAttempts' => []]);
+        }
+
+        $quizAttempts = $user->quizAttempts()
+            ->with('quiz:id,title')
+            ->latest('completed_at')
+            ->take(10)
+            ->get(['id', 'quiz_id', 'score', 'completed_at']);
+
+        return Inertia::render('Dashboard', [
+            'quizAttempts' => $quizAttempts,
+        ]);
+    }
+}
